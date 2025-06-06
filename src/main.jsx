a171e5ea47f64deb8d81d733f6c907caf61c9710
Diogo Silva <106630916+Diogo-Silva3@users.noreply.github.com>
2025-06-06 15:28:18 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
+import '@/index.css';
 import App from './App.jsx'
 import './index.css'
 import './i18n/i18n'
